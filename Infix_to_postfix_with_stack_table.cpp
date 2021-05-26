0dc53f54d9d0759a554aac5904d02af88fd2b814
MD Al Arman Sorker <73396847+mdalarmansorker@users.noreply.github.com>
2021-05-26 16:15:09 +0600

    Add files via upload
    This code will show the stack flow

---
+++ Infix_to_postfix_with_stack_table.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+using namespace std;
+//MD Al ARMAN SORKER - 200101059
+int main()
+{
+        string temp;
+        cin>>temp;
+        temp+=')';
+        string a="";
+        vector<char>amt;
+        int co=0;
+        stack<char>am;
+        am.push('(');
+        amt.push_back('(');
+        co++;
+        cout<<co<<": Stack: "<<'('<<endl;
+        cout<<"Expression: "<<endl;
+        int i=0;
+        int j=0;
+        for(i=0;i<temp.length();i++)
+        {
+            if((temp[i]>='A' and temp[i]<'Z') or (temp[i]>='a' and temp[i]<='z') or (temp[i]>='0' and temp[i]<='9'))
+            {
+                a+=temp[i];
+            }
+            else if(temp[i]=='(')
+            {
+                am.push(temp[i]);
+                amt.push_back(temp[i]);
+            }
+                
+            else if(temp[i]=='+' or temp[i]=='-' or temp[i]=='*' or temp[i]=='/' or temp[i]=='^')
+            {
+                while(!am.empty())
+                {
+                     if(am.top()=='^')
+                    {
+                        a+=am.top();
+                        am.pop();
+                        amt.pop_back();
+                    }
+                    else if((am.top()=='*' or am.top()=='/') and temp[i]!='^')
+                    {
+                        a+=am.top();
+                        am.pop();
+                        amt.pop_back();
+                    }
+                    else if((am.top()=='+' or am.top()=='-') and temp[i]!='^' and temp[i]!='*' and temp[i]!='/')
+                    {
+                        a+=am.top();
+                        am.pop();
+                        amt.pop_back();
+                    }
+                    else
+                    {
+                        am.push(temp[i]);
+                        amt.push_back(temp[i]);
+                        break;
+                    }
+                }
+            }
+            else if(temp[i]==')')
+            {
+                while(!am.empty() and am.top()!='(')
+                {
+                    a+=am.top();
+                    am.pop();
+                    amt.pop_back();
+                }
+                am.pop();
+                amt.pop_back();
+                
+            }
+            co++;
+                    string s(amt.begin(),amt.end());
+                    cout<<co<<": Stack: "<<s<<endl;
+                    cout<<"Expression: "<<a<<endl;
+        }
+       std::cout<<"Postfix Notation: "<<a<<endl;
+    return 0;
+}
